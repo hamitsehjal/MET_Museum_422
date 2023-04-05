b9f8f80d980eedf0890c99d4585392605dc303ef
hamitsehjal <hamitsehjal26@gmail.com>
2023-04-04 20:44:30 -0400

    added credentials -> moving to Deployment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
+*  WEB422 – Assignment 6
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: HAMIT SEHJAL Student ID: 139238208 Date: 2023-03-24
+*  Name: HAMIT SEHJAL Student ID: 139238208 Date: 2023-04-05
 *
 ********************************************************************************/
 
