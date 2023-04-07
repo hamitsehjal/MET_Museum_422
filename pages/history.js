a9cea3983615b7c5b5ee05bee3d256cae36db4bc
hamitsehjal <hamitsehjal26@gmail.com>
2023-04-07 14:50:36 -0400

    updated

--- pages/history.js
+++ pages/history.js
@@ -4,7 +4,7 @@ import { searchHistoryAtom } from "@/store"
 import { useRouter } from "next/router";
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Card, ListGroupItem, Button } from "react-bootstrap";
-import styles from '@/styles/History.module.css'
+import styles from '@/styles/history.module.css'
 import { removeFromHistory } from "@/lib/userData";
 import React from "react";
 
@@ -25,33 +25,13 @@ export default function History() {
     }
     const removeHistoryClicked = async (e, index) => {
         e.stopPropagation();
-        // setSearchHistory(current => {
-        //     let x = [...current];
-        //     x.splice(index, 1)
-        //     return x;
-        // });
+
         setSearchHistory(await removeFromHistory(searchHistory[index]))
 
     }
     return (<>
 
-        {/* {parsedHistory.length > 0 ? <ListGroup>{parsedHistory.map((historyItem, index) =>
-            <ListGroupItem className={styles.historyListItem} key={index} onClick={e => historyClicked(e, index)}>
-                {Object.keys(historyItem).map(key => (<>{key}: <strong>{historyItem[key]}</strong>&nbsp;</>))}
-                <Button className="float-end" variant="danger" size="sm"
-                    onClick={e => removeHistoryClicked(e, index)}>&times;</Button>
 
-            </ListGroupItem>)}
-        </ListGroup> :
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Nothing Here</Card.Title>
-                    <Card.Text>
-                        Try searching for some artwork
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        } */}
         {parsedHistory.length > 0 ? (
             <ListGroup>
                 {parsedHistory.map((historyItem, index) => (
